0cc7105d6c83eea4ab69878c2f32a7a1a62d2f97
Fabio Medeiros <kaspperfabio@gmail.com>
2026-02-08 16:23:03 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
   return (
     <div className="app-container">
       <Toaster position="top-center" richColors theme="dark" />
-      <CustomCursor />
       <Navbar />
 
       <main>
